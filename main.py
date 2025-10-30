7139f6b163c367fbd8524c28407b5c106ea1d97e
HaiVy-coder <buiduonghaivy@gmail.com>
2025-10-30 15:39:00 +0000

    bai6

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
-from guizero import App, Text, PushButton, Box, info
+from guizero import App, Text, PushButton, Box, Picture, info
 from random import randint
 cuaso = App(title = "tro choi di tim gian diep", width = 580, height = 600)
 chu = Text(cuaso, text = "truy tim gian diep")
 box = Box(cuaso, layout = "grid")
+
 hang = randint(0, 2)
 cot = randint(0, 2)
-def bamnut(h, c):
-    global hang, cot, button
+def bamnut(h, c, n):
+    global hang, cot
     if h == hang and c == cot:
         info("thong bao", "ban da tim thay gian diep!")
+        n.text = ""
+        n.image = "giandiep.png"
     else:
         info("thong bao", "sai roi, hay tim noi khac nao!")
 for i in range(3):
     for j in range(3):
-        button = PushButton(box, text = "?", grid = [i, j], width = 20, height = 10, command = bamnut, args = [i, j])
+        button = PushButton(box, text = "?", grid = [i, j], width = 20, height = 10, command = bamnut, args = [i, j, None])
+        button.update_command(bamnut, [i, j, button])
 cuaso.display()
